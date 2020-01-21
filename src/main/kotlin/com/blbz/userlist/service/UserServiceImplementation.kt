4d614c9b1b300c7f9bafc11f282fe827fdbdfa67
Rakshak D S <wasdlifeqecrlove@gmail.com>
2020-01-21 16:33:47 +0530

    [rak] user service implementation pushed

---
+++ src/main/kotlin/com/blbz/userlist/service/UserServiceImplementation.kt
@@ -0,0 +1,42 @@
+package com.blbz.userlist.service
+
+import com.blbz.userlist.entity.RegistrationDto
+import com.blbz.userlist.entity.User
+import com.blbz.userlist.exception.UserException
+import com.blbz.userlist.repository.IUserRepository
+import com.blbz.userlist.util.JwtGenerator
+import org.springframework.beans.BeanUtils
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+import javax.transaction.Transactional as Transactional1
+
+
+@Service
+abstract class UserServiceImplementation : IUserService {
+
+    private val user = User()
+    @Autowired
+    private val repository: IUserRepository? = null
+    @Autowired
+    private val encryption: BCryptPasswordEncoder? = null
+    @Autowired
+    private val generate: JwtGenerator? = null
+
+    @Transactional1
+    override fun register(info: RegistrationDto): User {
+        var user: User = repository!!.getUser(info.getEmail() as String)
+        return if (user == null) {
+            var userObj = User()
+            BeanUtils.copyProperties(info, userObj)
+            val epassword = encryption!!.encode(info.getPassword())
+            userObj.setPassword(epassword)
+            userObj.setVerified(false)
+            userObj = repository.save(user)
+            return userObj
+        } else {
+            throw UserException("Email id already exists")
+        }
+    }
+}
