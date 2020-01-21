52641ca6c03a594a77e955a25f9bcde4db78a8d4
Rakshak D S <wasdlifeqecrlove@gmail.com>
2020-01-21 16:28:46 +0530

    [rak] modified user repository

--- src/main/kotlin/com/blbz/userlist/repository/UserRepository.kt
+++ src/main/kotlin/com/blbz/userlist/repository/UserRepository.kt
@@ -1,10 +1,18 @@
 package com.blbz.userlist.repository
 
 import com.blbz.userlist.entity.User
-import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepository: JpaRepository<User, Long>{
+interface IUserRepository {
 
+    fun save(user: User): User
+
+    fun getUser(email: String): User
+
+    fun verify(id: Long?): Boolean
+
+    fun getUserById(id: Long?): User
+
+    val users: List<Any?>?
 }
