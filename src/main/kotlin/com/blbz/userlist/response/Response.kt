45b7fa7e3604fb71c57e8c988303eb6cd00dbd73
Rakshak D S <wasdlifeqecrlove@gmail.com>
2020-01-21 16:32:37 +0530

    [rak] global response pushed

---
+++ src/main/kotlin/com/blbz/userlist/response/Response.kt
@@ -0,0 +1,23 @@
+package com.blbz.userlist.response
+
+import lombok.Data
+
+
+@Data
+class Response {
+    private var message: String
+    private var statusCode: Int
+    private var obj: Any? = null
+    var details: List<String>? = null
+
+    constructor(message: String, statusCode: Int) {
+        this.message = message
+        this.statusCode = statusCode
+    }
+
+    constructor(message: String, statusCode: Int, obj: Any?) {
+        this.message = message
+        this.statusCode = statusCode
+        this.obj = obj
+    }
+}
