b1402719ee96d71ecc604291719edbb619839480
Rakshak D S <wasdlifeqecrlove@gmail.com>
2020-01-21 10:16:30 +0530

    [rak] test cases added

---
+++ src/test/kotlin/com/blbz/UserList/UserListApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.blbz.UserList
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserListApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
