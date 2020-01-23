ec5861ff94bc8b6bdda580ff40385d9a69ebccee
Rakshak D S <wasdlifeqecrlove@gmail.com>
2020-01-23 09:42:30 +0530

    [rak] refactored & modified User Service

--- src/main/kotlin/com/blbz/userlist/service/IUserService.kt
+++ src/main/kotlin/com/blbz/userlist/service/IUserService.kt
@@ -9,18 +9,6 @@ import org.springframework.stereotype.Service
 @Service
 interface IUserService {
 
-    fun login(info: LoginDto): User
-
     fun register(info: RegistrationDto): User
-
-    fun verify(token: String): Boolean
-
-    fun isUserExist(email: String): Boolean
-
-    fun update(information: PasswordUpdateDto, token: String): User
-
-    val users: List<User>
-
-    fun getSingleUser(token: String): User
 
 }
