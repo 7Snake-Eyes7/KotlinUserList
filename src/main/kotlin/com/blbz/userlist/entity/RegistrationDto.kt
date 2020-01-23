3970aa5dfda26dad57f9a95d444a8e89b3469e54
Rakshak D S <wasdlifeqecrlove@gmail.com>
2020-01-23 09:40:12 +0530

    [rak] refactored & modified registration dto

--- src/main/kotlin/com/blbz/userlist/entity/RegistrationDto.kt
+++ src/main/kotlin/com/blbz/userlist/entity/RegistrationDto.kt
@@ -1,11 +1,22 @@
 package com.blbz.userlist.entity
 
-import lombok.Data
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotEmpty
 
-@Data
-class RegistrationDto {
+data class RegistrationDto (
+
+    @get: NotBlank
+    var name:String = "",
+
+    @get: NotBlank
+    var email:String = "",
+
+    @get: NotEmpty
+    var mobileNumber:Long = 0,
+
+    @get: NotBlank
+    var password:String = ""
+) {
     fun getEmail(): Any {
         return email
     }
@@ -13,16 +24,4 @@ class RegistrationDto {
     fun getPassword(): CharSequence? {
         return password
     }
-
-    @get: NotBlank
-    var name:String = ""
-
-    @get: NotBlank
-    var email:String = ""
-
-    @get: NotEmpty
-    var mobileNumber:Long = 0
-
-    @get: NotBlank
-    var password:String = ""
 }
