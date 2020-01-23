76133aca77d3ba45e6ee578a1de565a2ba21e67f
Rakshak D S <wasdlifeqecrlove@gmail.com>
2020-01-23 09:40:35 +0530

    [rak] refactored & modified user

--- src/main/kotlin/com/blbz/userlist/entity/User.kt
+++ src/main/kotlin/com/blbz/userlist/entity/User.kt
@@ -1,35 +1,27 @@
 package com.blbz.userlist.entity
 
-import lombok.Data
-import java.time.LocalDateTime
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 
 @Entity
-@Data
-open class User {
-	open fun setPassword(epassword: String?): String {
-		return password
-	}
-
-	open fun setVerified(b: Boolean): Boolean {
-		return true
-	}
+data class User (
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	open var id:Long = 0
+	open var id:Long = 0,
 
-	open var name:String = ""
+	open var name:String = "",
 
-	open var email:String = ""
+	open var email:String = "",
 
-	open var mobileNumber:Long = 0
-
-	open var isVerified:Boolean = false
+	open var mobileNumber:Long = 0,
 
 	open var password:String = ""
 
+) {
+	fun setPassword(epassword: String?): String? {
+		return epassword
+	}
 }
