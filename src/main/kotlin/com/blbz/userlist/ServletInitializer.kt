e61cfecdddd7dc9c6e3a96759a114674e348f81d
Rakshak D S <wasdlifeqecrlove@gmail.com>
2020-01-27 15:41:37 +0530

    [rak] modified servlet initializer

--- src/main/kotlin/com/blbz/userlist/ServletInitializer.kt
+++ src/main/kotlin/com/blbz/userlist/ServletInitializer.kt
@@ -4,7 +4,7 @@ import com.blbz.userlist.UserListApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
-class ServletInitializer : SpringBootServletInitializer() {
+open class ServletInitializer : SpringBootServletInitializer() {
 
 	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
 		return application.sources(UserListApplication::class.java)
