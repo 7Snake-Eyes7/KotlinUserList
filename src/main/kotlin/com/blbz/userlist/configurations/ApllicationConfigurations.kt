6e435b95812e08a9f58b85799d34c880f2030aff
Rakshak D S <wasdlifeqecrlove@gmail.com>
2020-01-27 15:48:47 +0530

    [rak] refactored application configurations

--- src/main/kotlin/com/blbz/userlist/configurations/ApllicationConfigurations.kt
+++ src/main/kotlin/com/blbz/userlist/configurations/ApllicationConfigurations.kt
@@ -3,11 +3,10 @@ package com.blbz.userlist.configurations
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import springfox.documentation.swagger2.mappers.ModelMapper
 
 
 @Configuration
-class ApplicationConfigurations {
+open class ApplicationConfigurations {
     @get:Bean
     val passwordEncryption: BCryptPasswordEncoder
         get() = BCryptPasswordEncoder()
